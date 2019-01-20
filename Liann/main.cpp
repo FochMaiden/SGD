cc1a7fcbc619468525a341ed3b744df89420628a
Gosia Zdybel <fochmaiden@gosias-macbook-pro.local>
2019-01-20 12:25:14 +0100

    Initial Commit

---
+++ Liann/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Liann
+//
+//  Created by Gosia Zdybel on 20/01/2019.
+//  Copyright © 2019 Liann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
